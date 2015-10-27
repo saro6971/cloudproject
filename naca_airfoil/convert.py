df6b5fc7428107bab17abff39d14876ed4addef3
saro6971 <sam.ronnlund@gmail.com>
2015-10-27 11:43:56 +0000

    a

--- naca_airfoil/convert.py
+++ naca_airfoil/convert.py
@@ -3,7 +3,7 @@ import subprocess
 from subprocess import call
 
 def convert(num_nodes):
-        for filename in os.listdir('/home/emil/Project/naca_airfoil/msh'):
+        for filename in os.listdir('/home/ubuntu/cloudproject/naca_airfoil/msh'):
                 if filename.endswith(str(num_nodes) + ".msh"):
                         name = "dolfin-convert " + "/home/ubuntu/cloudproject/naca_airfoil/msh/" + filename + " /home/ubuntu/cloudproject/naca_airfoil/msh/" + filename + ".xml"
                         subprocess.call(name, shell=True)
